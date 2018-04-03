3928bfc46eddaef46a1c4654bbb7d97641f1d130
Cherise <cnh3qa@virginia.edu>
2018-04-02 22:59:58 -0400

    change login link from php to html
    chelsea changed the extension back to html since she got rid of her php so file needs to be html

--- index.php
+++ index.php
@@ -115,7 +115,7 @@
                          -->
                          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                             <ul class="nav navbar-nav navbar-right">
-                                <li><a href="login_view.php">Log In</a></li>
+                                <li><a href="login_view.html">Log In</a></li>
                                 <li class="dropdown">
                                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Social<span class="caret"></span></a>
                                     <ul class="dropdown-menu">
